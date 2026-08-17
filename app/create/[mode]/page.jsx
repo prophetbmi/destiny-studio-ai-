a4ee35f001abccb4efba8ac23eca513838bbd101
prophetbmi <bmiprophet@gmail.com>
2026-08-17 22:11:49 +0200

    Update page.jsx

--- app/create/[mode]/page.jsx
+++ app/create/[mode]/page.jsx
@@ -125,9 +125,6 @@ export default function CreatePage({ params }) {
         throw new Error(data.error || "Erreur inconnue");
       }
 
-      if (data._debugHistory) {
-  alert("DEBUG historique: " + data._debugHistory);
-      }
       setResult(data);
 
       try {
